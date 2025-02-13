627acd9842c672e628b8dc0bf597905b872581d7
alejandra marino <alejamarino123@gmail.com>
2025-02-12 21:48:56 -0500

    Add About markup and style

--- mlearn/src/components/About/About.jsx
+++ mlearn/src/components/About/About.jsx
@@ -1,1 +1,20 @@
 import "./About.css";
+import logo from "../../assets/mlearnLogo.svg";
+function About() {
+  return (
+    <section className="about">
+      <img className="about__image" src={logo} alt="logo" />
+      <div className="about__content">
+        <h2 className="about__title">About</h2>
+        <p className="about__description">
+          This blog is a collection of theory, code snippets, and visuals to
+          deepen your understanding of machine learning. The content is divided
+          into categories, such as supervised learning, unsupervised learning,
+          and reinforcement learning. Each post is tagged with relevant topics
+          and algorithms, and you can search for posts by title.
+        </p>
+      </div>
+    </section>
+  );
+}
+export default About;
