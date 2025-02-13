8dbaec80a094a53c9a97c0d6082363edc41fcbdb
alejandra marino <alejamarino123@gmail.com>
2025-02-12 21:55:17 -0500

    add footer markup and styles

--- mlearn/src/components/Footer/Footer.jsx
+++ mlearn/src/components/Footer/Footer.jsx
@@ -1,4 +1,12 @@
 import "./Footer.css";
-function Footer() {}
-
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__copyrigth">
+        © 2024 Alejandra Mariño, Powered by OpenML API
+      </p>
+      <p className="footer__social">github</p>
+    </footer>
+  );
+}
 export default Footer;
