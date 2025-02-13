41c56ab7d05b9a219b12f6eeaa6545b4be5688e4
alejandra marino <alejamarino123@gmail.com>
2025-02-13 14:42:06 -0500

    add preloader

--- mlearn/src/components/Preloader/Preloader.jsx
+++ mlearn/src/components/Preloader/Preloader.jsx
@@ -1,1 +1,11 @@
 import "./Preloader.css";
+
+function Preloader() {
+  return (
+    <div className="circle-preloader">
+      <p className="circle-preloader__text">Loading...</p>
+    </div>
+  );
+}
+
+export default Preloader;
