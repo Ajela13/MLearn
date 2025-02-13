2d78b5b3d8b0f7930543532decbb6e060d729e44
alejandra marino <alejamarino123@gmail.com>
2025-02-12 21:50:45 -0500

    add PostCard markup and style

--- mlearn/src/components/PostCard/PostCard.jsx
+++ mlearn/src/components/PostCard/PostCard.jsx
@@ -1,1 +1,31 @@
 import "./PostCard.css";
+import photo from "../../assets/background.png";
+
+function ItemCard() {
+  return (
+    <li className="card">
+      <div className="card__header">
+        <img className="card__image" src={photo} alt="image" />
+        <p className="card__date">Card Date</p>
+        <h2 className="card__name">Card Title</h2>
+        <p className="card__description">
+          Card Description tryui iu gbhnju g hyu guyb gvbuh gyuh igyubhu gyu b
+          Card Description tryui iu gbhnju g hyu guyb gvbuh gyuh igyubhu gyu b
+          Card Description tryui iu gbhnju g hyu guyb gvbuh gyuh igyubhu gyu b
+          Card Description tryui iu gbhnju g hyu guyb gvbuh gyuh igyubhu gyu b
+          Card Description tryui iu gbhnju g hyu guyb gvbuh gyuh igyubhu gyu b
+          Card Description tryui iu gbhnju g hyu guyb gvbuh gyuh igyubhu gyu b
+          Card Description tryui iu gbhnju g hyu guyb gvbuh gyuh igyubhu gyu b
+          Card Description tryui iu gbhnju g hyu guyb gvbuh gyuh igyubhu gyu b
+          Card Description tryui iu gbhnju g hyu guyb gvbuh gyuh igyubhu gyu b
+        </p>
+        <button className="card__save-btn"></button>
+      </div>
+      <div className="card__footer">
+        <p className="card__author">Card Author</p>
+      </div>
+    </li>
+  );
+}
+
+export default ItemCard;
