b222dc14a573382777b997d2331063d8fb16eb84
alejandra marino <alejamarino123@gmail.com>
2025-02-14 12:33:54 -0500

    create loginModal markup and style

--- mlearn/src/components/LoginModal/LoginModal.jsx
+++ mlearn/src/components/LoginModal/LoginModal.jsx
@@ -1,1 +1,74 @@
 import "./LoginModal.css";
+
+import { useEffect, useState } from "react";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import { useContext } from "react";
+import { ModalContext } from "../../contexts/ModalContext";
+
+function LoginModal({ isOpen }) {
+  const {} = useContext(ModalContext);
+  const { handleRegisterClick } = useContext(ModalContext);
+
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      setData({ email: "", password: "" });
+    }
+  }, [isOpen]);
+
+  //  const handleSubmit = (e) => {
+  //    e.preventDefault();
+  //    handleLoginSubmit(data);
+  //  };
+
+  return (
+    <ModalWithForm
+      title="Log in"
+      buttonText="Log in"
+      isOpen={isOpen}
+      //   onSubmit={handleSubmit}
+      redirectText="or Sign Up"
+      clickHandler={handleRegisterClick}
+    >
+      <label htmlFor="login-email" className="modal__label">
+        Email
+        <input
+          id="login-email"
+          type="email"
+          value={data.email}
+          className="modal__input"
+          placeholder="Email"
+          onChange={handleChange}
+          name="email"
+        />
+      </label>
+
+      <label htmlFor="login-password" className="modal__label">
+        Password
+        <input
+          id="login-password"
+          type="password"
+          value={data.password}
+          className="modal__input"
+          placeholder="Password"
+          onChange={handleChange}
+          name="password"
+        />
+      </label>
+    </ModalWithForm>
+  );
+}
+
+export default LoginModal;
