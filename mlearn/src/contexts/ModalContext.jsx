1538001a51ced67d0d02e424c10fa608e5a4e91a
alejandra marino <alejamarino123@gmail.com>
2025-02-21 15:51:55 -0500

    create modalContext

---
+++ mlearn/src/contexts/ModalContext.jsx
@@ -0,0 +1,42 @@
+import React, { createContext, useState } from "react";
+
+export const ModalContext = createContext();
+
+export const ModalProvider = ({ children }) => {
+  const [selectedCard, setSelectedCard] = useState({});
+  const [activeModal, setActiveModal] = useState("");
+
+  const closeActiveModal = () => {
+    setActiveModal("");
+  };
+
+  const handleAddClick = () => setActiveModal("add-post");
+  const handleLoginClick = () => setActiveModal("Login");
+  const handleRegisterClick = () => setActiveModal("register");
+  const handleUpdateProfileClick = () => setActiveModal("update profile");
+  const handleCardClick = (card) => {
+    setActiveModal("preview");
+    setSelectedCard(card);
+  };
+  const handleConfirmationDeleteClick = () => {
+    setActiveModal("confirmation");
+  };
+
+  return (
+    <ModalContext.Provider
+      value={{
+        activeModal,
+        closeActiveModal,
+        handleAddClick,
+        handleLoginClick,
+        handleRegisterClick,
+        handleUpdateProfileClick,
+        handleCardClick,
+        handleConfirmationDeleteClick,
+        selectedCard,
+      }}
+    >
+      {children}
+    </ModalContext.Provider>
+  );
+};
