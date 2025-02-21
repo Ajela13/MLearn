035d3f440ba7f4f042d1d4fa5dd7f39c9af12fea
alejandra marino <alejamarino123@gmail.com>
2025-02-21 15:54:55 -0500

    create postcard markup and style

--- mlearn/src/components/PostCard/PostCard.jsx
+++ mlearn/src/components/PostCard/PostCard.jsx
@@ -1,24 +1,26 @@
 import "./PostCard.css";
 import photo from "../../assets/background.png";
+import { ModalContext } from "../../contexts/ModalContext";
+import { useContext } from "react";
+import { usePost } from "../../contexts/PostContext";
+import SafeHtmlContent from "../SafeHtmlContent/SafeHtmlContent";
 
 function PostCard() {
+  const { handleCardClick } = useContext(ModalContext);
+  const { postData } = usePost();
+  console.log(postData.post);
+
+  const onCardClick = () => {
+    handleCardClick();
+  };
   return (
-    <li className="card">
+    <li className="card" onClick={onCardClick}>
       <div className="card__header">
-        <img className="card__image" src={photo} alt="image" />
         <p className="card__date">Card Date</p>
-        <h2 className="card__name">Card Title</h2>
-        <p className="card__description">
-          Card Description tryui iu gbhnju g hyu guyb gvbuh gyuh igyubhu gyu b
-          Card Description tryui iu gbhnju g hyu guyb gvbuh gyuh igyubhu gyu b
-          Card Description tryui iu gbhnju g hyu guyb gvbuh gyuh igyubhu gyu b
-          Card Description tryui iu gbhnju g hyu guyb gvbuh gyuh igyubhu gyu b
-          Card Description tryui iu gbhnju g hyu guyb gvbuh gyuh igyubhu gyu b
-          Card Description tryui iu gbhnju g hyu guyb gvbuh gyuh igyubhu gyu b
-          Card Description tryui iu gbhnju g hyu guyb gvbuh gyuh igyubhu gyu b
-          Card Description tryui iu gbhnju g hyu guyb gvbuh gyuh igyubhu gyu b
-          Card Description tryui iu gbhnju g hyu guyb gvbuh gyuh igyubhu gyu b
-        </p>
+        <h2 className="card__name">{postData.title}</h2>
+        <div className="card__description">
+          <SafeHtmlContent html={postData.post} />
+        </div>
         <button className="card__save-btn"></button>
       </div>
       <div className="card__footer">
