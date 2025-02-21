eae383413a7736234e44ae10cee66262933e98ca
alejandra marino <alejamarino123@gmail.com>
2025-02-21 16:05:09 -0500

    add link to profile, tasks and modlcontxt

--- mlearn/src/components/NavBar/NavBar.jsx
+++ mlearn/src/components/NavBar/NavBar.jsx
@@ -1,17 +1,31 @@
 import "./NavBar.css";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { useTextColor } from "../../contexts/TextColorContext";
+import { ModalContext } from "../../contexts/ModalContext";
 
 function NavBar() {
   const textColor = useTextColor();
-
+  const { handleAddClick, handleLoginClick } = useContext(ModalContext);
   return (
     <div className="navbar">
       <ul className="navbar__links">
-        <li className="navbar__link">Add post</li>
-        <li className="navbar__link">Profile</li>
+        <li className="navbar__link" onClick={handleAddClick}>
+          Add post
+        </li>
+        <Link to="/profile" className="navbar__link-no-style-link">
+          <li className="navbar__link">Profile</li>
+        </Link>
+        <Link to="/tasks" className="navbar__link-no-style-link">
+          <li className="navbar__link">Tasks</li>
+        </Link>
       </ul>
       <div className="navbar__auth">
-        <button className="navbar__SignIn" style={{ borderColor: textColor }}>
+        <button
+          className="navbar__SignIn"
+          style={{ borderColor: textColor }}
+          onClick={handleLoginClick}
+        >
           Sign in
         </button>
       </div>
