6e2bb0c599ee26c948c72841367a2d238f3de134
alejandra marino <alejamarino123@gmail.com>
2025-02-21 16:09:13 -0500

    add App markup and style

---
+++ mlearn/src/components/App/App.jsx
@@ -0,0 +1,35 @@
+import React, { useContext } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
+import PostModal from "../PostModal/PostModal";
+import Profile from "../Profile/Profile";
+import AddPostModal from "../AddPostModal/AddPostModal";
+import LoginModal from "../LoginModal/LoginModal";
+import RegisterModal from "../RegisterModal/RegisterModal";
+import DataSets from "../DataSets/DataSets";
+import { ModalContext } from "../../contexts/ModalContext.jsx";
+
+const App = () => {
+  const { activeModal } = useContext(ModalContext);
+
+  return (
+    <div className="page">
+      <div className="page__content">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/datasets" element={<DataSets />} />
+        </Routes>
+        <Footer />
+      </div>
+      <AddPostModal isOpen={activeModal === "add-post"} />
+      <PostModal isOpen={activeModal == "preview"} />
+      <LoginModal isOpen={activeModal == "Login"} />
+      <RegisterModal isOpen={activeModal == "register"} />
+    </div>
+  );
+};
+
+export default App;
