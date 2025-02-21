41efa8f55f2109f411e30261acd0350f0a25af84
alejandra marino <alejamarino123@gmail.com>
2025-02-21 15:59:48 -0500

    add link markup and style

--- mlearn/src/components/Header/Header.jsx
+++ mlearn/src/components/Header/Header.jsx
@@ -1,17 +1,19 @@
 import "./Header.css";
-import NavBar from "../NavBar/NavBar";
-import logo from "../../assets/mlearnLogo.svg";
+import { Link } from "react-router-dom";
+import logo from "../../assets/mlearnNoBack.svg";
 import { useTextColor } from "../../contexts/TextColorContext";
-
+import NavBar from "../NavBar/NavBar";
 function Header() {
   const textColor = useTextColor();
   return (
     <header className="header" style={{ color: textColor }}>
       <div className="header__content">
-        <div className="header__logo-container">
-          <img className="header__logo" src={logo} alt="logo" />
-          <p className="header__title">MLearn</p>
-        </div>
+        <Link to="/" className="header__link header__link_no-style-link">
+          <div className="header__logo-container">
+            <img className="header__logo" src={logo} alt="logo" />
+            <p className="header__title">MLearn</p>
+          </div>
+        </Link>
         <NavBar />
       </div>
     </header>
