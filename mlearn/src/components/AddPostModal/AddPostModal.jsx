b3859674ad83ec34b0fe495bc61119a19c3e2527
alejandra marino <alejamarino123@gmail.com>
2025-02-21 16:08:07 -0500

    add Add post modal markup and style

--- mlearn/src/components/AddPostModal/AddPostModal.jsx
+++ mlearn/src/components/AddPostModal/AddPostModal.jsx
@@ -1,1 +1,57 @@
-import "AddPostModal.css";
+import "./AddPostModal.css";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import Editor from "../Editor/Editor";
+import { useEffect } from "react";
+import { usePost } from "../../contexts/PostContext";
+
+function AddPostModal({ isOpen }) {
+  const { postData, setPostData } = usePost();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Formulario enviado:", postData);
+    // setPostData({ title: "", post: "<h2>Start here...</h2>" });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPostData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      console.log(postData);
+      setPostData((prevData) => ({
+        ...prevData,
+      }));
+    }
+  }, [isOpen, setPostData]);
+
+  return (
+    <ModalWithForm
+      title="New Post"
+      buttonText="Add Post"
+      isOpen={isOpen}
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor="title" className="modal__label">
+        Title
+        <input
+          id="title"
+          type="text"
+          value={postData.title}
+          className="modal__input"
+          placeholder="Title"
+          onChange={handleChange}
+          name="title"
+        />
+      </label>
+      <Editor isOpen={isOpen} />
+    </ModalWithForm>
+  );
+}
+
+export default AddPostModal;
