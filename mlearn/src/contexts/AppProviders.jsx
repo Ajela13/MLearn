206e7e745e1468cb0070d243c02ee9722e3d03b1
alejandra marino <alejamarino123@gmail.com>
2025-02-21 15:54:29 -0500

    add all post,text and modal provdrs

--- mlearn/src/contexts/AppProviders.jsx
+++ mlearn/src/contexts/AppProviders.jsx
@@ -1,7 +1,14 @@
 import { TextColorProvider } from "./TextColorContext";
-
+import { ModalProvider } from "./ModalContext";
+import { PostProvider } from "./PostContext";
 const AppProviders = ({ children }) => {
-  return <TextColorProvider>{children}</TextColorProvider>;
+  return (
+    <PostProvider>
+      <ModalProvider>
+        <TextColorProvider>{children}</TextColorProvider>;
+      </ModalProvider>
+    </PostProvider>
+  );
 };
 
 export default AppProviders;
