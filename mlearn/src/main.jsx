0c10ad8ce376ff53dac8a04bd0f6c4468b6fc7cc
alejandra marino <alejamarino123@gmail.com>
2025-02-21 15:50:58 -0500

    update main,providers, routers and strictmode

--- mlearn/src/main.jsx
+++ mlearn/src/main.jsx
@@ -1,10 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
 import "./index.css";
+import AppProviders from "./contexts/AppProviders";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <AppProviders>
+        <App />
+      </AppProviders>
+    </BrowserRouter>
+  </StrictMode>
 );
