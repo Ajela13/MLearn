90fb751d2eda47b17f3d17b105e4f934a11591e0
alejandra marino <alejamarino123@gmail.com>
2025-03-09 21:11:33 -0500

    create task list

---
+++ mlearn/src/components/TaskList/TaskList.jsx
@@ -0,0 +1,33 @@
+import "./TaskList.css";
+import { useEffect } from "react";
+import useTaskStore from "../../Store/Store";
+import { getTasksByType } from "../../utils/openMlApi";
+import TaskCard from "../TaskCard/TaskCard";
+
+function TaskList() {
+  const { selectedTaskType, tasks } = useTaskStore();
+  useEffect(() => {
+    if (!selectedTaskType) return;
+    getTasksByType(selectedTaskType.id);
+  }, [selectedTaskType]);
+
+  return (
+    <div className="taskList">
+      <h2 className="taskList__title">
+        Tasks for {selectedTaskType?.name || "..."}
+      </h2>
+
+      {tasks.length > 0 ? (
+        <div className="taskList__list">
+          {tasks.map((task) => (
+            <TaskCard key={task.id} task={task} />
+          ))}
+        </div>
+      ) : (
+        <p>No tasks type selected yet.</p>
+      )}
+    </div>
+  );
+}
+
+export default TaskList;
