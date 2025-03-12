5d88c4d541709e1d957cbc6ac04cb1e97821b7f3
alejandra marino <alejamarino123@gmail.com>
2025-03-11 21:45:21 -0500

    remove Appprovider

--- mlearn/src/main.jsx
+++ mlearn/src/main.jsx
@@ -3,14 +3,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
 import "./index.css";
-import AppProviders from "./contexts/AppProviders";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <AppProviders>
-        <App />
-      </AppProviders>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
