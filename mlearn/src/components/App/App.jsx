37e823e064cc293bc75da2ee1755b20804c5ef93
alejandra marino <alejamarino123@gmail.com>
2025-03-12 21:52:05 -0500

    feat: add protected route and fetchData api

--- mlearn/src/components/App/App.jsx
+++ mlearn/src/components/App/App.jsx
@@ -1,7 +1,8 @@
+import "./App.css";
 import React from "react";
 import { useModalStore } from "../../Store/UseModalStore.js";
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
+import { useEffect } from "react";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 import PostModal from "../PostModal/PostModal";
@@ -10,16 +11,32 @@ import AddPostModal from "../AddPostModal/AddPostModal";
 import LoginModal from "../LoginModal/LoginModal";
 import RegisterModal from "../RegisterModal/RegisterModal";
 import Tasks from "../Tasks/Tasks.jsx";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute.jsx";
+import { getPosts } from "../../utils/api.js";
+import { usePostStore } from "../../Store/UsePostStore.js";
+import { useAuthStore } from "../../Store/UseAuthStore.js";
 
 const App = () => {
   const { activeModal } = useModalStore();
+  const { setPostData } = usePostStore();
+  const { isLoggedIn } = useAuthStore();
+  useEffect(() => {
+    getPosts().then(setPostData);
+  }, []);
 
   return (
     <div className="page">
       <div className="page__content">
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute isLoggedIn={isLoggedIn}>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/tasks" element={<Tasks />} />
         </Routes>
         <Footer />
