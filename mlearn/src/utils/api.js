5fd2e272fa392a2a8a5da977e352e2bfe644f0a2
alejandra marino <alejamarino123@gmail.com>
2025-03-13 14:04:03 -0500

    feat: add fake api

---
+++ mlearn/src/utils/api.js
@@ -0,0 +1,24 @@
+const fakeDatabase = [
+  {
+    id: "65f7368dfb74bd6a92114c85",
+    title: "Some news post",
+    post: "<h2>Start here...</h2>",
+    savedBy: ["fake-id"], // List of users who saved it
+    createdBy: ["fake-id"],
+    date: "2025-03-11",
+  },
+  {
+    id: "65f7368dfkjb74bd6a92114c85",
+    title: "Another news post",
+    post: "<p>This is some content...</p>",
+    savedBy: [],
+    createdBy: ["fake-id"],
+    date: "2025-03-10",
+  },
+];
+
+export function getPosts() {
+  return new Promise((resolve) => {
+    resolve(fakeDatabase);
+  });
+}
