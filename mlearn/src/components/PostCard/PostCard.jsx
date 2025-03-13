e843676f33c16cab77a462b2d819ba5d3040f4b6
alejandra marino <alejamarino123@gmail.com>
2025-03-13 17:07:47 -0500

    refact: refactor postCard structure

--- mlearn/src/components/PostCard/PostCard.jsx
+++ mlearn/src/components/PostCard/PostCard.jsx
@@ -9,13 +9,13 @@ function PostCard({ post }) {
   if (!post || !post.title || !post.post) {
     return null;
   }
-
+  console.log(currentUser);
   const { handleCardClick } = useModalStore();
   const { toggleSavePost } = usePostStore();
   const isSaved = post.savedBy.includes(currentUser.id);
 
   const onCardClick = () => {
-    handleCardClick();
+    handleCardClick(post);
   };
   return (
     <li className="card">
@@ -24,15 +24,17 @@ function PostCard({ post }) {
         onClick={() => toggleSavePost(post.id, currentUser.id)}
         hidden={!isLoggedIn}
       ></button>
-      <div className="card__header" onClick={onCardClick}>
-        <p className="card__date">{post.date}</p>
-        <h2 className="card__title">{post.title}</h2>
-        <div className="card__description">
-          <SafeHtmlContent html={post.post} />
+      <div className="card__content" onClick={onCardClick}>
+        <div className="card__header">
+          <p className="card__date">{post.date}</p>
+          <h2 className="card__title">{post.title}</h2>
+          <div className="card__description">
+            <SafeHtmlContent html={post.post} />
+          </div>
         </div>
-      </div>
-      <div className="card__footer">
-        <p className="card__author">Card Author</p>
+        <div className="card__footer">
+          <p className="card__author">{post.createdBy}</p>
+        </div>
       </div>
     </li>
   );
