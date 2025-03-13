d493ec97732f834063d34466de116a251c6f5c10
alejandra marino <alejamarino123@gmail.com>
2025-03-12 20:33:00 -0500

    feat: add show btn visible 3 renders

--- mlearn/src/components/Main/Main.jsx
+++ mlearn/src/components/Main/Main.jsx
@@ -3,8 +3,13 @@ import SearchForm from "../SearchForm/SearchForm";
 import PostList from "../PostList/PostList";
 import About from "../About/About";
 import "./Main.css";
+import { usePostStore } from "../../Store/UsePostStore";
 
 function Main() {
+  const { postData, visibleCount, increaseVisibleCount } = usePostStore();
+
+  const visiblePosts = postData.slice(0, visibleCount);
+
   return (
     <main className="main">
       <div className="main__overlay"></div>
@@ -17,8 +22,12 @@ function Main() {
         <SearchForm />
         <div className="main__results">
           <h2 className="main__results-title">Search results</h2>
-          <PostList />
-          <button className="main__show-btn">Show more</button>
+          <PostList posts={visiblePosts} />
+          {postData.length > 3 && visibleCount < postData.length && (
+            <button className="main__show-btn" onClick={increaseVisibleCount}>
+              Show more
+            </button>
+          )}
         </div>
         <About />
       </div>
