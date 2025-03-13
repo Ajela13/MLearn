d656385973f7321df6ddd4979894b9bc28a60734
alejandra marino <alejamarino123@gmail.com>
2025-03-13 14:03:02 -0500

    add currentUser structure , register , login and logout

--- mlearn/src/Store/UseAuthStore.js
+++ mlearn/src/Store/UseAuthStore.js
@@ -1,9 +1,34 @@
 import { create } from "zustand";
+import { authorize, checkToken } from "../utils/auth";
 
 export const useAuthStore = create((set) => ({
-  currentUser: null,
+  currentUser: {
+    id: null,
+    name: "",
+    password: "",
+    email: "",
+  },
   isLoggedIn: false,
 
-  setCurrentUser: (userData) => set({ currentUser: userData }),
-  setLoggedIn: () => set((state) => ({ isLoggedIn: !state.isLoggedIn })),
+  register: async (name, email, password) => {
+    try {
+      const response = await authorize(email, password);
+      const userData = await checkToken(response.token);
+      set({ currentUser: { ...userData.data, name }, isLoggedIn: true });
+    } catch (error) {
+      console.error("Registration failed", error);
+    }
+  },
+
+  login: async (email, password) => {
+    try {
+      const response = await authorize(email, password);
+      const userData = await checkToken(response.token);
+      set({ currentUser: userData.data, isLoggedIn: true });
+    } catch (error) {
+      console.error("Login failed", error);
+    }
+  },
+
+  logout: () => set({ currentUser: null, isLoggedIn: false }),
 }));
